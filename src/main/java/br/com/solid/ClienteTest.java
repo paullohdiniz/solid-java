cb7bb8142dfe3330c0e54ac73aacd23c7f9c208d
Paulo Diniz <paulo.diniz@emphasysgroup.com.br>
2018-09-14 21:27:52 -0300

    Modified .gitignore

---
+++ src/main/java/br/com/solid/ClienteTest.java
@@ -0,0 +1,14 @@
+package br.com.solid;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
